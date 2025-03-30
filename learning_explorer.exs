faf0c23c465cbcbb01cc4d37796eefb135405648
conf8o <41929738+conf8o@users.noreply.github.com>
2025-03-30 21:32:08 +0900

    rough

--- learning_explorer.exs
+++ learning_explorer.exs
@@ -21,3 +21,28 @@ DF.filter(mountains, elevation > mean(elevation))
 # ------
 # テキストのやつ
 # ------
+
+
+### DataFrame ###
+
+df = DataFrame.new(
+  index: ["row1", "row2", "row3"],
+  column1: [1, 2, 3],
+  column2: [4, 5, 6]
+)
+
+DataFrame.print(df)
+
+# df.indexに相当するものは無さそう。
+# 新しく列を追加したいときは、DataFrame.put や DataFrame.mutate
+
+
+### Series ###
+
+series_1 = Series.from_list([7, 8])
+IO.inspect(series_1)
+
+# Seriesには、indexに相当するものは無い(https://hexdocs.pm/explorer/Explorer.Series.html#module-series-queries)
+
+
+# 所感: pandasのDataFrameの読みにくい演算の挙動を排除しているイメージ
